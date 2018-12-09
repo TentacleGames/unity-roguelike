8cdfc2fee5a3514a420e0b015ab8c6bb4ebdd369
Unknown <shtabel@live.com>
2018-12-09 23:24:54 +0300

    realtime moving character
    added realtime moving
    added camera following

---
+++ Assets/Scripts/CameraControlScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControlScript : MonoBehaviour {
+
+   private GameObject player;
+
+	// Update is called once per frame
+	void LateUpdate () {
+      if (player != null)
+      {
+         if (player.transform.position.x != transform.position.x || player.transform.position.y != transform.position.y)
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+      }
+      else
+      {
+         FindPlayer();
+      }
+	}
+
+   private void FindPlayer()
+   {
+      if (player == null)
+      {
+         player = GameObject.FindGameObjectWithTag("Player");
+      }
+   }
+
+}
